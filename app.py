95a9d81a03434a6e72c2d37fea4df6d9ae99b388
taleluz <117649980+taleluz@users.noreply.github.com>
2022-12-27 10:35:50 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -5,118 +5,129 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
 # Create SQLITE Database - myDb
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///Library.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Library.sqlite3'
 app.config['SECRET_KEY'] = "random string"
- 
+
 db = SQLAlchemy(app)
 
-#Models
+# Models
+
+
 class Books(db.Model):
-    id = db.Column("book_id",db.Integer, primary_key=True)
+    id = db.Column("book_id", db.Integer, primary_key=True)
     bookName = db.Column(db.String(30))
     author = db.Column(db.String(30))
     yearPublished = db.Column(db.Integer)
     typeLoan = db.Column(db.Integer)
-    books= db.relationship('Loans', backref='books')
+    book_status = db.Column(db.String(30))
+    books = db.relationship('Loans', backref='books')
 
-    def __init__(self, bookName, author, yearPublished, typeLoan):
-        self.bookName= bookName
+    def __init__(self, bookName, author, yearPublished, typeLoan, book_status):
+        self.bookName = bookName
         self.author = author
         self.yearPublished = yearPublished
         self.typeLoan = typeLoan
+        self.book_status = book_status
 
 
 class Customers(db.Model):
-    id = db.Column("customer_id",db.Integer, primary_key=True)
+    id = db.Column("customer_id", db.Integer, primary_key=True)
     customerName = db.Column(db.String(80))
     city = db.Column(db.String(120))
     age = db.Column(db.Integer)
     customerStatus = db.Column(db.String(10))
-    customers= db.relationship('Loans', backref='customers')
-   
-    def __init__(self, customerName, city, age,customerStatus):
-        self.customerName= customerName
+    customers = db.relationship('Loans', backref='customers')
+
+    def __init__(self, customerName, city, age, customerStatus):
+        self.customerName = customerName
         self.city = city
         self.age = age
         self.customerStatus = customerStatus
 
+
 class Loans(db.Model):
-    id = db.Column("loan_id",db.Integer, primary_key=True)
-    CustomerID=db.Column(db.Integer,db.ForeignKey('customers.customer_id'))
-    BookID = db.Column(db.Integer,db.ForeignKey('books.book_id'))
+    id = db.Column("loan_id", db.Integer, primary_key=True)
+    CustomerID = db.Column(db.Integer, db.ForeignKey('customers.customer_id'))
+    BookID = db.Column(db.Integer, db.ForeignKey('books.book_id'))
     Loandate = db.Column(db.String(50))
     Returndate = db.Column(db.String(50))
     loan_status = db.Column(db.String(50))
-    
+    loan_activate = db.Column(db.String(50))
 
-    def __init__(self,CustomerID, BookID, Loandate, Returndate,loan_status):
+    def __init__(self, CustomerID, BookID, Loandate, Returndate, loan_status, loan_activate):
         self.CustomerID = CustomerID
         self.BookID = BookID
         self.Loandate = Loandate
         self.Returndate = Returndate
-        self.loan_status =loan_status 
-       
-#Book Views
-@app.route('/books/<id>',methods = ['GET','DELETE'])
-@app.route('/books/',methods = ['POST','GET'])
-def books(id=-1): 
-    if request.method == 'POST': 
+        self.loan_status = loan_status
+        self.loan_activate = loan_activate
+
+# Book Views
+
+
+@app.route('/books/<id>', methods=['GET', 'PUT'])
+@app.route('/books/', methods=['POST', 'GET'])
+def books(id=-1):
+    if request.method == 'POST':
         request_data = request.get_json()
         bookName = request_data["bookName"]
         author = request_data["author"]
         yearPublished = request_data["yearPublished"]
         typeLoan = request_data["typeLoan"]
-        newBook= Books(bookName,author,yearPublished,typeLoan)
-        db.session.add (newBook)
+        book_status = request_data["book_status"]
+        newBook = Books(bookName, author, yearPublished, typeLoan, book_status)
+        db.session.add(newBook)
         db.session.commit()
         return ("A book was added")
-      
 
     if request.method == 'GET':
-        res=[]
+        res = []
         for book in Books.query.all():
-              res.append({"book_id":book.id,
-              "bookName":book.bookName,
-              "author":book.author,
-              "yearPublished":book.yearPublished,
-              "typeLoan":book.typeLoan})
+              res.append({"book_id": book.id,
+              "bookName": book.bookName,
+              "author": book.author,
+              "yearPublished": book.yearPublished,
+              "typeLoan": book.typeLoan,
+              "book_status": book.book_status})
         return (json.dumps(res))
 
-    if request.method == 'DELETE':
-        del_book= Books.query.get(id)
-        db.session.delete(del_book)
-        db.session.commit()
-        return "A book was deleted"
+    if request.method == 'PUT':
+           request_data = request.get_json()
+           UpdBook = Books.query.get(id)
+           if UpdBook:
+                UpdBook.book_status = request_data["book_status"]
+                db.session.commit()
+           return "A book was update "
 
 
-#Customers Views
-@app.route('/customers/<id>',methods = ['DELETE','PUT'])
-@app.route('/customers/',methods = ['POST','GET'])
-def customers(id=-1): 
-    if request.method == 'POST': 
+# Customers Views
+@app.route('/customers/<id>', methods=['DELETE', 'PUT'])
+@app.route('/customers/', methods=['POST', 'GET'])
+def customers(id=-1):
+    if request.method == 'POST':
         request_data = request.get_json()
         customerName = request_data["customerName"]
         city = request_data["city"]
         age = request_data["age"]
         customerStatus = request_data["customerStatus"]
-        newCustomer= Customers(customerName,city,age, customerStatus)
-        db.session.add (newCustomer)
+        newCustomer = Customers(customerName, city, age, customerStatus)
+        db.session.add(newCustomer)
         db.session.commit()
         return ("A customer was added")
-     
 
     if request.method == 'GET':
-        res=[]
+        res = []
         for customer in Customers.query.all():
-            res.append({"customer_id":customer.id,
-            "customerName":customer.customerName,
-            "city":customer.city,"age":customer.age,
-            "customerStatus":customer.customerStatus})
+            res.append({"customer_id": customer.id,
+            "customerName": customer.customerName,
+            "city": customer.city, "age": customer.age,
+            "customerStatus": customer.customerStatus})
         return (json.dumps(res))
 
     if request.method == 'DELETE':
-        del_customer= Customers.query.get(id)
+        del_customer = Customers.query.get(id)
         db.session.delete(del_customer)
         db.session.commit()
         return "A customer was deleted"
@@ -125,47 +136,53 @@ def customers(id=-1):
             request_data = request.get_json()
             updCustomer = Customers.query.get(id)
             if updCustomer:
-                updCustomer.customerStatus=request_data["customerStatus"]
+                updCustomer.customerStatus = request_data["customerStatus"]
                 db.session.commit()
             return "A customer was update "
 
 
-#Loans Views
-@app.route('/loans/<id>',methods=['DELETE'])
-@app.route('/loans/',methods=['POST','GET'])
+# Loans Views
+@app.route('/loans/<id>', methods=['DELETE', 'PUT'])
+@app.route('/loans/', methods=['POST', 'GET'])
 def loans(id=-1):
-    if request.method == 'POST': 
+    if request.method == 'POST':
         request_data = request.get_json()
         CustomerID = request_data["CustomerID"]
         BookID = request_data["BookID"]
         Loandate = request_data["Loandate"]
         Returndate = request_data["Returndate"]
         loan_status = request_data["loan_status"]
-        newLoan= Loans(CustomerID,BookID,Loandate,Returndate,loan_status)
-        db.session.add (newLoan)
+        loan_activate = request_data["loan_activate"]
+        newLoan = Loans(CustomerID, BookID, Loandate,
+                        Returndate, loan_status, loan_activate)
+        db.session.add(newLoan)
         db.session.commit()
         return ("A loan was added")
-       
-    if request.method == 'GET': 
-       res=[]
-       for loan,book,customer in db.session.query(Loans,Books,Customers).join(Books).join(Customers):
-                res.append({"loan_id":loan.id,
-                        "CustomerID":loan.CustomerID,
-                        "BookID":loan.BookID,
-                        "typeLoan":book.typeLoan,
-                        "bookName":book.bookName,
-                        "Loandate":loan.Loandate,
-                        "Returndate":loan.Returndate,
-                        "loan_status":loan.loan_status,
-                        "customerName":customer.customerName})
-       return (json.dumps(res))   
 
-        
-    if request.method == 'DELETE':
-        del_loan= Loans.query.get(id)
-        db.session.delete(del_loan)
-        db.session.commit()
-        return "A book was returned"
+    if request.method == 'GET':
+       res = []
+       for loan, book, customer in db.session.query(Loans, Books, Customers).join(Books).join(Customers):
+                res.append({"loan_id": loan.id,
+                        "CustomerID": loan.CustomerID,
+                        "BookID": loan.BookID,
+                        "book_id": book.id,
+                        "typeLoan": book.typeLoan,
+                        "bookName": book.bookName,
+                        "Loandate": loan.Loandate,
+                        "Returndate": loan.Returndate,
+                        "loan_status": loan.loan_status,
+                        "loan_activate": loan.loan_activate,
+                        "customerName": customer.customerName})
+       return (json.dumps(res))
+
+
+    if request.method == 'PUT':
+            request_data = request.get_json()
+            upd_loan = Loans.query.get(id)
+            if upd_loan:
+                upd_loan.loan_activate =request_data["loan_activate"]
+                db.session.commit()
+            return "A loan was update "
 
 
 if __name__ == '__main__':
